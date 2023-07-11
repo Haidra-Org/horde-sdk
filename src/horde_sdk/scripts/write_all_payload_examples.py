91fc0f69db7bdafedb22e10f2e739f1ceb38635e
tazlin <tazlin.on.github@gmail.com>
2023-07-10 22:28:41 -0400

    fix: fix the write_all* scripts

--- src/horde_sdk/scripts/write_all_payload_examples.py
+++ src/horde_sdk/scripts/write_all_payload_examples.py
@@ -5,7 +5,9 @@ from horde_sdk.generic_api.utils.swagger import SwaggerParser
 
 
 def main(*, test_data_path: Path | None = None):
-    ai_horde_swagger_doc = SwaggerParser(get_ai_horde_swagger_url()).get_swagger_doc()
+    ai_horde_swagger_doc = SwaggerParser(
+        swagger_doc_url=get_ai_horde_swagger_url(),
+    ).get_swagger_doc()
     if not test_data_path:
         test_data_path = (
             Path(__file__).parent.parent.parent.parent / "tests" / "test_data" / "ai_horde_api" / "example_payloads"
