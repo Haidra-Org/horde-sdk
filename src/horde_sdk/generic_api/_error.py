48397acc29680cbbf8e26a08e9b1308a3298e2e9
tazlin <tazlin.on.github@gmail.com>
2023-07-10 22:17:56 -0400

    refactor: brings `RequestErrorResponse` up to status quo

--- src/horde_sdk/generic_api/_error.py
+++ src/horde_sdk/generic_api/_error.py
@@ -1,8 +1,5 @@
-from collections.abc import Callable
-
 from typing_extensions import override
 
-from horde_sdk.consts import HTTPStatusCode
 from horde_sdk.generic_api.apimodels import BaseResponse
 
 
@@ -14,31 +11,10 @@ class RequestErrorResponse(BaseResponse):
 
     message: str = ""
 
+    object_data: object = None
+    """This is a catch all for any additional data that may be returned by the API relevant to the error."""
+
     @override
     @classmethod
     def get_api_model_name(cls) -> str | None:
         return "RequestError"
-
-    @override
-    @classmethod
-    def get_expected_http_status_codes(cls) -> dict[HTTPStatusCode, Callable]:
-        def dummy_func(x):
-            return x
-
-        return {
-            HTTPStatusCode.BAD_REQUEST: dummy_func,
-            HTTPStatusCode.UNAUTHORIZED: dummy_func,
-            HTTPStatusCode.FORBIDDEN: dummy_func,
-            HTTPStatusCode.NOT_FOUND: dummy_func,
-            HTTPStatusCode.METHOD_NOT_ALLOWED: dummy_func,
-            HTTPStatusCode.NOT_ACCEPTABLE: dummy_func,
-            HTTPStatusCode.REQUEST_TIMEOUT: dummy_func,
-            HTTPStatusCode.CONFLICT: dummy_func,
-            HTTPStatusCode.GONE: dummy_func,
-            HTTPStatusCode.UNPROCESSABLE_ENTITY: dummy_func,
-            HTTPStatusCode.TOO_MANY_REQUESTS: dummy_func,
-            HTTPStatusCode.INTERNAL_SERVER_ERROR: dummy_func,
-            HTTPStatusCode.NOT_IMPLEMENTED: dummy_func,
-            HTTPStatusCode.SERVICE_UNAVAILABLE: dummy_func,
-            HTTPStatusCode.GATEWAY_TIMEOUT: dummy_func,
-        }
