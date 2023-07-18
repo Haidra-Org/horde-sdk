14795ef029fe054e8d34627e3c12f0baa4b99cb3
tazlin <tazlin.on.github@gmail.com>
2023-07-18 12:06:50 -0400

    tests: allows run first/last behavior with tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,18 +24,25 @@ def simple_image_gen_n_requests() -> ImageGenerateAsyncRequest:
     )
 
 
-def pytest_collection_modifyitems(items: list) -> None:
-    """Modifies test items in place to ensure test modules run in a given order."""
-    MODULE_ORDER = ["tests_generic", "test_utils", "test_dynamically_check_apimodels"]
-    # `test.scripts` must run first because it downloads the legacy database
+def pytest_collection_modifyitems(items):  # type: ignore
+    """Modifies test items to ensure test modules run in a given order."""
+    MODULES_TO_RUN_FIRST = ["tests_generic", "test_utils", "test_dynamically_check_apimodels"]
+
+    MODULES_TO_RUN_LAST = [
+        "tests.ai_horde_api.test_ai_horde_api_calls",
+    ]
     module_mapping = {item: item.module.__name__ for item in items}
 
-    sorted_items = items.copy()
+    sorted_items = []
 
-    # Iteratively move tests of each module to the end of the test queue
-    for module in MODULE_ORDER:
-        sorted_items = [it for it in sorted_items if module_mapping[it] != module] + [
-            it for it in sorted_items if module_mapping[it] == module
-        ]
+    for module in MODULES_TO_RUN_FIRST:
+        sorted_items.extend([item for item in items if module_mapping[item] == module])
+
+    sorted_items.extend(
+        [item for item in items if module_mapping[item] not in MODULES_TO_RUN_FIRST + MODULES_TO_RUN_LAST],
+    )
+
+    for module in MODULES_TO_RUN_LAST:
+        sorted_items.extend([item for item in items if module_mapping[item] == module])
 
     items[:] = sorted_items
