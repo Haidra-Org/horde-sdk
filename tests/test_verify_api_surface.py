723d180f82d014303231e6d739cb62cd9113a394
tazlin <tazlin.on.github@gmail.com>
2024-04-17 19:14:56 -0400

    tests: generalize surface checking to other types

--- tests/test_verify_api_surface.py
+++ tests/test_verify_api_surface.py
@@ -2,11 +2,33 @@ def test_all_ai_horde_api_models_imported() -> None:
     import horde_sdk.ai_horde_api.apimodels
     import horde_sdk.meta
 
-    unimported_classes, missing_imports = horde_sdk.meta.any_unimported_classes(horde_sdk.ai_horde_api.apimodels)
+    unimported_classes, missing_imports = horde_sdk.meta.any_unimported_classes(
+        horde_sdk.ai_horde_api.apimodels,
+        horde_sdk.HordeAPIObject,
+    )
 
     missing_import_names = {cls.__name__ for cls in missing_imports}
     assert not unimported_classes, (
-        "The following classes are not imported in the `__init__.py` of the apimodels "
+        "The following HordeAPIObjects are not imported in the `__init__.py` of the apimodels "
+        f" namespace: : {missing_imports}"
+        f"\n\nMissing import names: {missing_import_names}"
+    )
+
+
+def test_all_api_data_objects_imported() -> None:
+
+    import horde_sdk.ai_horde_api.apimodels
+    import horde_sdk.meta
+
+    unimported_classes, missing_imports = horde_sdk.meta.any_unimported_classes(
+        horde_sdk.ai_horde_api.apimodels,
+        horde_sdk.HordeAPIDataObject,
+    )
+
+    missing_import_names = {cls.__name__ for cls in missing_imports}
+
+    assert not unimported_classes, (
+        "The following HordeAPIDataObjects are not imported in the `__init__.py` of the apimodels "
         f" namespace: : {missing_imports}"
         f"\n\nMissing import names: {missing_import_names}"
     )
@@ -16,11 +38,14 @@ def test_all_ratings_api_models_imported() -> None:
     import horde_sdk.ratings_api.apimodels  # noqa: I001
     import horde_sdk.meta
 
-    unimported_classes, missing_imports = horde_sdk.meta.any_unimported_classes(horde_sdk.ratings_api.apimodels)
+    unimported_classes, missing_imports = horde_sdk.meta.any_unimported_classes(
+        horde_sdk.ratings_api.apimodels,
+        horde_sdk.HordeAPIObject,
+    )
 
     missing_import_names = {cls.__name__ for cls in missing_imports}
     assert not unimported_classes, (
-        "The following classes are not imported in the `__init__.py` of the apimodels "
+        "The following HordeAPIObjects are not imported in the `__init__.py` of the apimodels "
         f" namespace: : {missing_imports}"
         f"\n\nMissing import names: {missing_import_names}"
     )
