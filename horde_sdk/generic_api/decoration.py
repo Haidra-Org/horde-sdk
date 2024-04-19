864bda76adee4afb8362bc6600ea2aaf2fbeff3a
tazlin <tazlin.on.github@gmail.com>
2024-04-19 18:54:19 -0400

    tests: `Unequatable` decorator
    See also 9e3d88a0f19eb9402c02c74a8ccecdeb1e5f289f

--- horde_sdk/generic_api/decoration.py
+++ horde_sdk/generic_api/decoration.py
@@ -12,7 +12,14 @@ def Unhashable(cls: type[T]) -> type[T]:
     Returns:
         Any: The unhashable class.
     """
+
     cls._unhashable = True  # type: ignore
+
+    def __hash__(self) -> int:  # type: ignore # noqa: ANN001
+        raise NotImplementedError(f"Hashing is not implemented for {cls.__name__}")
+
+    cls.__hash__ = __hash__  # type: ignore
+
     return cls
 
 
@@ -28,3 +35,37 @@ def is_unhashable(obj: type | Any) -> bool:  # noqa: ANN401
     cls = obj.__class__ if not isinstance(obj, type) else obj
 
     return getattr(cls, "_unhashable", False)
+
+
+def Unequatable(cls: type[T]) -> type[T]:
+    """A decorator that makes a class unequatable
+
+    Args:
+        cls (type[T]): The class to make unequatable
+
+    Returns:
+        type[T]: The unequatable class
+    """
+
+    cls._unequatable = True  # type: ignore
+
+    def __eq__(self, other: Any) -> bool:  # type: ignore # noqa: ANN001, ANN401
+        raise NotImplementedError(f"Equality is not implemented for {cls.__name__}")
+
+    cls.__eq__ = __eq__  # type: ignore
+
+    return cls
+
+
+def is_unequatable(obj: type | Any) -> bool:  # noqa: ANN401
+    """Check if an object is unequatable.
+
+    Args:
+        obj (Any): The object to check.
+
+    Returns:
+        bool: True if the object is unequatable, False otherwise.
+    """
+    cls = obj.__class__ if not isinstance(obj, type) else obj
+
+    return getattr(cls, "_unequatable", False)
