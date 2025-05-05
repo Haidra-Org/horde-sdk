8d7643c47ba6667b78d4396508b0e36801076de6
tazlin <tazlin.on.github@gmail.com>
2025-05-05 10:59:53 -0400

    docs: addtl. clarification for metadata classes

--- horde_sdk/generic_api/metadata.py
+++ horde_sdk/generic_api/metadata.py
@@ -15,6 +15,9 @@ from strenum import StrEnum
 class GenericHeaderFields(StrEnum):
     """`StrEnum` for data that may be passed in the header of a request.
 
+    Maps the python object's field name which matches to a comparable header. This implies that fields with these
+    names are *always* passed in the header of a request.
+
     Pass this in to a GenericClient initializer if implementing a new API. See an existing API's `metadata.py` module.
     """
 
@@ -37,12 +40,18 @@ class GenericAcceptTypes(StrEnum):
 class GenericPathFields(StrEnum):
     """`StrEnum` for data that may be passed as part of a URL path (before the query string).
 
+    Used to replace path parameters like `/api/v1/{path_field}` with the value of the field. This implies that fields
+    with these names are *always* passed in the path of a request.
+
     Pass this in to a GenericClient initializer if implementing a new API. See an existing API's `metadata.py` module.
     """
 
 
 class GenericQueryFields(StrEnum):
     """`StrEnum` for data that may be passed as part of a URL query (after the `?`).
+
+    Used to replace query parameters like `/api/v1/resource?query_field=value` with the value of the field.
+    This implies that fields with these names are *always* passed in the query of a request.
 
     Pass this in to a GenericClient initializer if implementing a new API. See an existing API's `metadata.py` module.
     """
